74f70d661cb5195b1b3d5f2aa340940a9ee841ea
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-06-04 11:17:45 -0300

    Secutiry Measures - IP and Session
    Session IP and current IP comparison in every request - avoid external IP acess
    Residual session information cleaned at login

--- MedVoll/MedVoll.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MedVoll/MedVoll.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,11 +21,13 @@ namespace MedVoll.Web.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger, UserManager<IdentityUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         /// <summary>
@@ -114,6 +116,14 @@ namespace MedVoll.Web.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
+                    //LIMPEZA DE DADOS DA SESSÃO RESIDUAL
+                    HttpContext.Session.Clear();
+
+                    //REEMISSÃO DO COOKIE DE AUTENTICAÇÃO APÓS LOGIN
+                    var user = await _userManager.FindByEmailAsync(Input.Email);
+                    await _signInManager.SignOutAsync();
+                    await _signInManager.SignInAsync(user, Input.RememberMe);
+
                     //DEFININDO VARIÁVEL DA SESSÃO - CHAMADA DE VollMedCard - QUE SERÁ USADA NA VIEW
                     HttpContext.Session.SetString("VollMedCard", "1234.4567.7890.1234");
                     _logger.LogInformation("User logged in.");
--- MedVoll/MedVoll.Web/Controllers/BaseController.cs
+++ MedVoll/MedVoll.Web/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
 ﻿using MedVoll.Web.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -6,8 +7,24 @@ namespace MedVoll.Web.Controllers
 {
     public class BaseController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        //CONSTRUTOR DA CLASSE
+        public BaseController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        //MÉTODO EXECUTADO SEMPRE QUE UMA REQUISIÇÃO É FEITA AOS CONTROLLERS
         public override void OnActionExecuting(ActionExecutingContext context)
         {
+            //SE FOR UMA REQUISIÇÃO DE UM IP DIFERENTE DO IP DA SESSÃO, REDIRECIONA
+            //PARA LOGIN - JÁ DESLOGADO
+            if (!CheckSessionSecurity())
+            {
+                context.Result = new RedirectResult("./Login");
+            }
+
             ViewData["Especialidades"] = GetEspecialidades();
             //MOSTRA NA VIEW O DADO DA VARIÁVEL DE SESSÃO VollMedCard
             ViewData["VollMedCard"] = HttpContext.Session.GetString("VollMedCard");
@@ -19,5 +36,26 @@ namespace MedVoll.Web.Controllers
             var especialidades = (Especialidade[])Enum.GetValues(typeof(Especialidade));
             return especialidades.ToList();
         }
+
+        //MÉTODO QUE COMPARA O IP ATUAL COM O IP DA SESSÃO - PARA EVITAR ATAQUE DE IPs
+        //EXTERNOS À SESSÃO COM COOKIES SEQUESTRADOS
+        //MÉTODO USADO EM OnActionExecuting
+        private bool CheckSessionSecurity()
+        {
+            var currentIp = HttpContext.Connection.RemoteIpAddress.ToString();
+            var sessionIp = HttpContext.Session.GetString("IpAddress");
+
+            if (sessionIp != null && sessionIp != currentIp)
+            {
+                HttpContext.Session.Clear();
+                _signInManager.SignOutAsync();
+                return false;
+            }
+
+            HttpContext.Session.SetString("IpAddress", currentIp);
+            return true;
+        }
+
+
     }
 }
